8fd37fbc41d24d71e972217cd7df93805d4605df
ink1852 <hjgim07@gmail.com>
2025-12-27 01:04:57 +0900

    update bg

--- js/background.js
+++ js/background.js
@@ -1,25 +1,16 @@
-const bgImg = document.getElementById("bg");
+const images = [
+    "pexels-1.jpg",
+    "pexels-2.jpg",
+    "pexels-3.jpg",
+    "pexels-4.jpg",
+]
 
-// 740 ~ 780 사이 랜덤 높이
-const minHeight = 740;
-const maxHeight = 780;
-const randomHeight =
-  Math.floor(Math.random() * (maxHeight - minHeight + 1)) + minHeight;
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-// 캐시 방지용 랜덤 값
-const randomSeed = Math.floor(Math.random() * 100000);
+const bgImage = document.createElement("img");
 
-// Lorem Picsum URL
-const imageURL = `https://picsum.photos/1280/${randomHeight}?random=${randomSeed}`;
+bgImage.src = `img/${chosenImage}`;
 
-// 이미지 적용
-bgImg.src = imageURL;
+document.body.appendChild(bgImage);
 
-// 화면 전체 배경처럼 보이게 스타일 적용
-bgImg.style.position = "fixed";
-bgImg.style.top = "0";
-bgImg.style.left = "0";
-bgImg.style.width = "100vw";
-bgImg.style.height = "100vh";
-bgImg.style.objectFit = "cover";
-bgImg.style.zIndex = "-1";
+bgImage.classList.add("background-img-style");
